965898dc08a470cd285d9037dd37800cc3e7ca1c
Mihir Gupta <mihirgupta0900@gmail.com>
2021-08-08 20:00:29 +0530

    undo some header changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div style={{ zIndex: 9999 }} className="sticky top-0 bg-bgc w-full">
-      <div className="flex md:flex-row flex-col justify-between mx-auto text-gray-700 dark:text-gray-200 h-28 items-center">
+      <div className="flex md:flex-row flex-col justify-between mx-auto md:w-1/2 text-gray-700 dark:text-gray-200 h-28 items-center">
         <div className="flex-shrink-0 px-8 py-4 flex flex-row items-center justify-between">
           <Link href="/">
             <a className="text-2xl font-semibold font-heading mx-auto tracking-widest text-gray-900 uppercase rounded-lg dark:text-white focus:outline-none focus:shadow-outline">
