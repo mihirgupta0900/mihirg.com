08a2ced716b1383410548b960df97b97cded8917
Mihir Gupta <mihirgupta0900@gmail.com>
2023-07-07 11:32:01 +0530

    chore: upgrade to next 13

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   images: {},
+  async rewrites() {
+    return [
+      {
+        source: "/home",
+        destination: "/",
+      },
+    ];
+  },
 };
