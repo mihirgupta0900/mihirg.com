58b50fd1ac16d702509e1d90363b96b1ad3192e8
Mihir Gupta <mihirgupta0900@gmail.com>
2023-08-28 00:20:14 +0530

    feat(home): adds articles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from "../components/Layout";
 import Subscribe from "../components/Subscribe";
 import { getAllBlogs } from "../utils";
 import { Post } from "../utils/interfaces";
+import Link from "next/link";
 
 interface Props {
   posts: Post[];
@@ -17,14 +18,20 @@ const Home: FC<Props> = ({ posts }) => {
           Hey! I'm Mihir Gupta
         </h1>
         <p className="mt-4 text-xl">
-          Welcome to my home in the cloud. I build software, write articles
-          (WIP) and share what I learn from books, people and experiences. I
+          Welcome to my home in the cloud. I build software, write articles and
+          share what I learn from books, people and experiences. I currently
           work as the CTO of <a href="https://coinvise.co">Coinvise.</a>
         </p>
 
-        <p className="mt-4 text-xl">
-          This site is still a work in progress. Untill then feel free to reach
-          me on{" "}
+        <div className="my-10">
+          <h2 className="font-heading font-medium text-4xl mb-4">Articles</h2>
+          <ul className="list-inside list-disc">
+            <ArticleItem title="Fork your Postgres DB" href="/post/fork_db" />
+            <ArticleItem title="Tools I use" href="/post/tools" />
+          </ul>
+        </div>
+        <p className="text-xl">
+          Feel free to reach me on{" "}
           <a href="https://twitter.com/0900mihir">Twitter {/*(@0900mihir)*/}</a>{" "}
           or{" "}
           <a href="mailto:mihirgupta0900@gmail.com">
@@ -45,6 +52,17 @@ const Home: FC<Props> = ({ posts }) => {
         </ul>
       </div> */}
     </Layout>
+  );
+};
+
+const ArticleItem: FC<{
+  title: string;
+  href: string;
+}> = ({ href, title }) => {
+  return (
+    <li className="my-1">
+      <Link href={href}>{title}</Link>
+    </li>
   );
 };
 
